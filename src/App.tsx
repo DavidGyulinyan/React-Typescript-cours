d38790e65a4c076d035289efb5b52901203c00bc
davit <davidgyulinyan@gmail.com>
2024-02-12 00:49:01 +0400

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
+
+import Counter from "./components/Counter"
 import Heading from "./components/Heading"
+import Section from "./components/Section"
 
 function App() {
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
     <Heading title={"This is a fuckin title"}/>
+    <Section title={"Section's Title"}>
+      This is Section
+    </Section>
+    <Counter setCount={setCount}>
+      Count is {count}
+    </Counter>
     </>
   )
 }
