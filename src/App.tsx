7fcab8dedbe26bf5b082ab4279a715ff3d2d9c40
davit <davidgyulinyan@gmail.com>
2024-02-12 14:44:03 +0400

    implemented List component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import Counter from "./components/Counter"
 import Heading from "./components/Heading"
 import Section from "./components/Section"
+import List from "./components/List"
 
 function App() {
   const [count, setCount] = useState<number>(0)
@@ -17,6 +18,9 @@ function App() {
     <Counter setCount={setCount}>
       Count is {count}
     </Counter>
+    <List items={['Cofee', 'Tacos', 'Code']}
+    render={(item: string) => <span className="gold">{item} </span>}
+    />
     </>
   )
 }
